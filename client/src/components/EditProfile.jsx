b34c3d5f02b8bfce824697dfbd5be175024e1127
Anmol Pal <anmol111pal@gmail.com>
2023-08-01 19:47:51 +0530

    Removed unnecessary import - useEffect

--- client/src/components/EditProfile.jsx
+++ client/src/components/EditProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import CryptoJS from "crypto-js";
 import Navbar from "./Navbar";
 import { useAuth } from "../AuthContext";
